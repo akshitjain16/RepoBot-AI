47b04071845b1103692d80d1c952052c08ed232f
Akshit jain <gang.akshitjain@gmail.com>
2025-01-02 16:44:25 +0530

    add sign-in, sign-up, and dashboard pages; implement utility functions and UI components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
 
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
@@ -15,10 +18,12 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <ClerkProvider>
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
