0268278343965c2c7cb0323ad667c9af72384602
Akshit jain <gang.akshitjain@gmail.com>
2025-01-04 02:07:11 +0530

    add CommitLog component to display project commits; update DashboardPage to include CommitLog; implement getCommits API endpoint

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import useProject from "@/hooks/use-project";
 import { ExternalLink, Github } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import CommitLog from "./commit-log";
 
 const DashboardPage = () => {
   const { project } = useProject();
@@ -45,7 +46,7 @@ const DashboardPage = () => {
       </div>
 
       <div className="mt-8"></div>
-      commmitLog
+      <CommitLog />
     </div>
   );
 };
--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -4,12 +4,12 @@ import { pollCommits } from "@/lib/github"
 
 export const projectRouter = createTRPCRouter({
   createProject: protectedProcedure.input(
-   z.object({
-    name: z.string().min(1, "Project name is required"),
-    githubUrl: z.string().url("Must be a valid GitHub URL"),
-    GithubToken: z.string().optional()
-  })
-  ).mutation(async({ctx, input}) => {
+    z.object({
+      name: z.string().min(1, "Project name is required"),
+      githubUrl: z.string().url("Must be a valid GitHub URL"),
+      GithubToken: z.string().optional()
+    })
+  ).mutation(async ({ ctx, input }) => {
     const project = await ctx.db.project
       .create({
         data: {
@@ -22,14 +22,14 @@ export const projectRouter = createTRPCRouter({
           },
         },
       })
-       try {
+    try {
       await pollCommits(project.id)
     } catch (error) {
       console.error("Failed to poll commits:", error)
     }
     return project
   }),
-  getProjects: protectedProcedure.query(async ({ctx}) => {
+  getProjects: protectedProcedure.query(async ({ ctx }) => {
     const projects = await ctx.db.project.findMany({
       where: {
         userToProjects: {
@@ -41,5 +41,14 @@ export const projectRouter = createTRPCRouter({
       }
     })
     return projects
+  }),
+  getCommits: protectedProcedure.input(z.object({
+    projectId: z.string()
+  })).query(async ({ ctx, input }) => {
+    return ctx.db.commit.findMany({
+      where: {
+        projectId: input.projectId
+      }
+    })
   })
 })
